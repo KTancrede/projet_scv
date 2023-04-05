4d3d77e08872815f71a22232bb4a7d26e73d75a7
KTancrede <tancredekowal@outlook.fr>
2023-04-05 23:19:57 +0200

    ca bug

--- gestCommit.c
+++ gestCommit.c
@@ -1,4 +1,3 @@
-#include "gestCommit.h"
 #include "commit.h"
 #include "stdio.h"
 #include "stdlib.h"
@@ -7,6 +6,8 @@
 #include "hash.h"
 #include "lcc.h"
 #include "git.h"
+#include "gestCommit.h"
+
 
 //Crée le répertoire caché .refs (s’il n’existe pas déjà), puis crée les fichiers master et HEAD (vides)
 void initRefs () {
@@ -108,4 +109,149 @@ void myGitCommit(char* branch_name, char* message){
     createUpdateRef(branch_name,hashc);
     createUpdateRef("HEAD",hashc);
     system ( "rm .add") ;
+}
+//void initBranch() qui crée le fichier caché .current branch contenant la chaı̂ne de caractères ”master”.
+void initBranch(){
+    FILE * f = fopen ( ".current_branch" ,"w");
+    fputs("master",f ) ;
+    fclose(f);
+}
+//vérifie l’existence d’une branche
+int branchExists(char*branch){
+    List * refs = listdir(".refs");
+    return searchList (refs,branch )!= NULL ;
+}
+//crée une référence appelée branch, qui pointe vers le même commit que la référence HEAD
+void createBranch(char* branch){
+    char * hash = getRef("HEAD");
+    createUpdateRef(branch,hash);
+}
+
+//lit le fichier caché .current branch pour retourner le nom de la branche courante
+char* getCurrentBranch(){
+    FILE * f = fopen ( ".current_branch","r");
+    char * buff = malloc(sizeof(char)*100);
+    fscanf (f, "%s",buff);
+    return buff;
+}
+
+char * hashToPathCommit(char * hash ){
+    char * buff = malloc (sizeof(char)*100) ;
+    sprintf (buff , "%s.c", hashToPath ( hash ));
+    return buff ;
+}
+
+void printBranch(char* branch){
+    char * commit_hash = getRef (branch);
+    Commit * c = ftc(hashToPathCommit(commit_hash));
+    while (c !=NULL){
+        if(commitGet(c,"message") != NULL )
+            printf( "%s −> %s \n",commit_hash,commitGet(c,"message"));
+        else
+            printf("%s \n",commit_hash);
+        if (commitGet(c ,"predecessor") != NULL){
+            commit_hash=commitGet(c,"predecessor");
+            c=ftc(hashToPathCommit(commit_hash));
+        }else{
+            c=NULL;
+        }
+    }
+}
+//qui contruit et retourne une liste  chaı̂née contenant le hash de tous les commits de la branche.
+List* branchList(char* branch){
+    List * L = initList();
+    char * commit_hash = getRef(branch);
+    Commit * c = ftc(hashToPathCommit(commit_hash));
+    while ( c != NULL ) {
+        insertFirst (L,buildCell(commit_hash));
+        if ( commitGet (c ,"predecessor") != NULL ) {
+            commit_hash = commitGet (c ,"predecessor");
+            c = ftc(hashToPathCommit(commit_hash));
+        }else{
+           c = NULL ; 
+        }
+    }
+    return L;
+}
+//renvoie la liste des hash des commits de toutes les branches (sans répétition)
+List *getAllCommits (){
+    List * L = initList();
+    List * content = listdir (".refs");
+    for ( Cell * ptr = * content ; ptr != NULL ; ptr = ptr-> next ) {
+        if ( ptr-> data [0] == '.' )
+            continue;
+        List * list = branchList ( ptr -> data ) ;
+        Cell * cell = * list ;
+        while ( cell != NULL ) {
+            if (searchList(L,cell -> data ) == NULL ) {
+                insertFirst (&L,buildCell(cell->data));
+            }
+        cell = cell -> next ;
+        }
+    }
+    return L ;
+}
+//permet de restaurer le worktree associé à un commit dont le hash est donné en paramètre
+void restoreCommit(char* hash_commit){
+    char * commit_path = hashToPathCommit ( hash_commit ) ;
+    Commit * c = ftc(commit_path);
+    char * tree_hash = strcat(hashToPath(commitGet(c ,"tree")) ,".t");
+    WorkTree * wt = ftwt(tree_hash);
+    restoreWorkTree ( wt ,".");
+}
+
+void myGitCheckoutBranch(char* branch){
+    //Change current_branch
+    FILE * f = fopen(".current_branch","w");
+    fprintf (f,"%s", branch ) ;
+    fclose(f);
+
+    char * hash_commit = getRef(branch);
+    createUpdateRef("HEAD",hash_commit);
+    restoreCommit(hash_commit);
+}
+//retourne une nouvelle liste contenant uniquement les éléments de L qui commencent par la chaı̂ne de caractères pattern
+List* filterList(List* L, char* pattern){
+    List * filtered =initList();
+    for( Cell * ptr = * L ; ptr != NULL ; ptr = ptr -> next ) {
+        char * c = strdup ( ptr->data);
+        c [strlen(pattern)] ='\0';
+        if ( strcmp (c , pattern ) == 0) {
+            insertFirst ( filtered , buildCell ( ptr -> data ) ) ;
+        }
+        free ( c ) ;
+    }
+    return filtered ;
+}
+
+//Compte le nombre d'élément dans la liste
+int listSize(List l) {
+    int count = 0;
+    Cell* current = l;
+    while (current != NULL) {
+        count++;
+        current = current->next;
+    }
+    return count;
+}
+
+
+
+void myGitCheckoutCommit(char* pattern){
+    List * L = getAllCommits();
+    List * filtred_list = filterList(L,pattern);
+    if (listSize(filtred_list)==1){
+        char * commit_hash = (listGet(filtred_list , 0))-> data ;
+        createUpdateRef ("HEAD", commit_hash ) ;
+        restoreCommit ( commit_hash ) ;
+    } else {
+        if(listSize(filtred_list)==0){
+            printf ( "No pattern matching\n");
+        } else {
+            printf ( "Multipkle matching found: \n");
+            for ( Cell *ptr=*filtred_list;ptr!=NULL;ptr=ptr->next ) {
+                printf ( "-> %s \n" , ptr -> data);
+            }
+        }
+    }
 }
